6d97f1af3af4bf1b802c99463f5036f2bdb72b1c
Denick <1049485+apie@users.noreply.github.com>
2018-12-02 16:52:49 +0100

    Day 1

---
+++ 1a.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+total = 0
+with open('1.input', 'r') as in_list:
+  for l in in_list.readlines():
+    #print(l)
+    num = int(l.replace('+', ''))
+    #print(num)
+    total += num
+
+print('Total: {}'.format(total))
+  
+  
---
+++ 1b.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+from itertools import cycle
+
+total = 0
+reached = {}
+reached[total] = 1
+with open('1.input', 'r') as in_list:
+  in_list_lines = in_list.readlines()
+  for l in cycle(in_list_lines):
+    #print(l)
+    num = int(l.replace('+', ''))
+    #print(num)
+    total += num
+    if total not in reached:
+      reached[total] = 1
+    else:
+      print('Reached {} twice'.format(total))
+      break
+
+print('Total: {}'.format(total))
+  
+  
