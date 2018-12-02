fc510a28b2ed0e41e21cd8f32ebcb0f3eaad0d18
Denick <1049485+apie@users.noreply.github.com>
2018-12-02 17:51:37 +0100

    Day 2

---
+++ 2a.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+from collections import Counter
+#Get number of lines wich contain letters with a frequency of 2
+#Get number of lines wich contain letters with a frequency of 3
+#Multiply those numbers
+
+two_count = three_count = 0
+with open('2.input', 'r') as in_list:
+  in_list_lines = in_list.readlines()
+for l in in_list_lines:
+  counter = Counter(l)
+  #print(l)
+  #print(counter)
+  two_count += any([k for k, v in counter.items() if v == 2])
+  three_count += any([k for k, v in counter.items() if v == 3])
+
+print('Two: {}, three: {}'.format(two_count, three_count))
+print('Checksum: {}'.format(two_count * three_count))
+
+  
---
+++ 2b.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+#Calculate hamming distance for all combinations of lines
+#Find two lines that only differ by one character
+#Output the common characters in those two lines
+
+def hamming(s1, s2):
+  "Hamming distance. Sum of number of differing characters"
+  assert len(s1) == len(s2)
+  return sum(c1 != c2 for c1, c2 in zip(s1, s2))
+
+the_two_lines = []
+
+with open('2.input', 'r') as in_list:
+  in_list_lines = in_list.readlines()
+for l in in_list_lines:
+  #print(l)
+  #Compare with all the other lines (and itself)
+  for c in in_list_lines:
+    if hamming(l, c) == 1:
+      print(l)
+      the_two_lines.append(l)
+
+print('Common characters:', end=' ')
+print(''.join([c1 for c1, c2 in zip(the_two_lines[0], the_two_lines[1]) if c1 == c2 ]))
+
+
+
