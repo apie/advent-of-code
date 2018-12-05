5a12519dd05a8b34bc8caeb31be9de6a0b7f19c2
Denick <1049485+apie@users.noreply.github.com>
2018-12-05 17:15:04 +0100

    Complete day 5

---
+++ 05b.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+import pytest
+import string
+from operator import itemgetter
+
+def reduce_str(in_str):
+  finished = False
+  tmp_str = in_str
+  sub_result = tmp_str
+  while not finished:
+    for char in string.ascii_lowercase:
+      sub_result = sub_result.replace(char.lower()+char.upper(), '').replace(char.upper()+char.lower(), '')
+    if sub_result == tmp_str:
+      finished = True
+    tmp_str = sub_result
+  return tmp_str
+
+def find_smallest(in_line):
+  results = []
+  for char in string.ascii_lowercase:
+    results.append((char, len(reduce_str(in_line.replace(char, '').replace(char.upper(), '')))))
+  return sorted(results, key=itemgetter(1))[0]
+
+
+
+@pytest.fixture
+def example_input():
+  return 'dabAcCaCBAcCcaDA'
+
+def test_reduce_str(example_input):
+  reduced = reduce_str(example_input)
+  assert reduced == 'dabCBAcaDA'
+  assert len(reduced) == 10
+
+def test_find_smallest(example_input):
+  assert find_smallest(example_input) == ('c', 4)
+
+if __name__ == '__main__':
+  with open('05.input', 'r') as in_list:
+    print(find_smallest(in_list.readline().strip()))
+
+
