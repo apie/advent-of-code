df0040b582c67905716d9e5469d7e5f358315e69
Denick <1049485+apie@users.noreply.github.com>
2018-12-09 23:45:32 +0100

    Cleanup 08b

--- 08b.py
+++ 08b.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 import pytest
 
-
 def prep_input(in_lines):
   prepped_input = []
   for line in in_lines:
@@ -35,14 +34,6 @@ def parse_nodes(in_list, nodes=None, nr_iter=None):
   #never reached?
   return nodes
 
-def sum_meta(nodes, meta_total=0):
-  for node in nodes:
-    for m in node['meta']:
-      meta_total += m
-    if 'childs' in node:
-      meta_total = sum_meta(node['childs'], meta_total)
-  return meta_total
-
 def nodevalue(node):
   if len(node['childs']):
     return sum(nodevalue(node['childs'][m-1])
@@ -55,29 +46,10 @@ def example_input():
   return '''
   2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2
   '''
-  #1 3 0 1 9 1 2 3
-  #0 3 1 2 3 0 1 9
-  #0 3 1 2 3
 
 def test_most(example_input):
   prepped_input = prep_input(example_input.split('\n'))
-  print(prepped_input)
   nodes = parse_nodes(iter(prepped_input))
-  import pprint
-  pprint.pprint(nodes, depth=100)
-  assert nodes == [
-    dict(childs=[ #A
-        dict(meta=[10,11,12], childs=[]),#B
-        dict(childs=[#C
-          dict(meta=[99], childs=[])#D
-          ],
-          meta=[2],
-        )
-      ],
-      meta=[1,1,2],
-    )
-  ]
-  assert sum_meta(nodes) == 138
   assert nodevalue(nodes[0]) == 66 #A
   assert nodevalue(nodes[0]['childs'][0]) == 33 #B
   assert nodevalue(nodes[0]['childs'][1]['childs'][0]) == 99 #D
