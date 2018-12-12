af140e359e5934a4c148b41d098989b38d7e7a62
Denick <1049485+apie@users.noreply.github.com>
2018-12-12 21:25:04 +0100

    Update template

--- template.py
+++ template.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import pytest
+DAY=0
 
 def answer(in_lines):
   pass
@@ -13,7 +14,7 @@ def test_answer(example_input):
   assert answer(example_input.split('\n')) == 1
 
 if __name__ == '__main__':
-  with open('XX.input', 'r') as in_file:
+  with open('{:02}.input'.format(DAY), 'r') as in_file:
     print(answer(in_file.readlines()))
 
 
