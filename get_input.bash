61d29a0fd7390055e6ef66a2728515cd2646df5a
Denick <1049485+apie@users.noreply.github.com>
2019-12-13 12:32:34 +0100

    Update script to use absolute paths to read and save files
    Allow to pass days with leading zero

--- get_input.bash
+++ get_input.bash
@@ -1,13 +1,14 @@
 #!/bin/bash
-if [ ! -f cookie.txt ]
+DIR=$(dirname "$0")
+if [ ! -f "$DIR"/cookie.txt ]
 then
   echo 'No cookie found'
   exit 1
 fi
-COOKIE=`cat cookie.txt`
+COOKIE=$(cat "$DIR"/cookie.txt)
 YEAR=2019
-DAY=$1
+DAY=$(($1)) # Convert to int to strip leading zero
 if [ $# -gt 0 ]
 then
-  curl "https://adventofcode.com/$YEAR/day/$DAY/input" -H "Cookie: session=$COOKIE" > $YEAR/"$(printf "%.2d.input" "$DAY")"
+  curl "https://adventofcode.com/$YEAR/day/$DAY/input" -H "Cookie: session=$COOKIE" > "$DIR"/$YEAR/"$(printf "%.2d.input" "$DAY")"
 fi
