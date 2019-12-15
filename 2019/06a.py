22d63e7193cf2702869ebe5adabd2158c8d1e90c
Denick <1049485+apie@users.noreply.github.com>
2019-12-15 14:07:41 +0100

    use lut and parents for 06a

--- 2019/06a.py
+++ 2019/06a.py
@@ -1,30 +1,39 @@
 #!/usr/bin/env python3
+import datetime
 import pytest
 import sys
 import fileinput
 from os.path import splitext, abspath
 
 class Tree():
-    def __init__(self, data, level=0):
+    def __init__(self, data, level=0, parents=None):
+        if not parents:
+            parents = set()
         self.left = []
         self.right = []
         self.data = data
         self.level = level
+        self.parents = parents
     def add(self, data):
+        parents = set(self.parents)
+        parents.add(self.data)
+        t = Tree(data, self.level+1, parents)
         if not self.left:
-            self.left = Tree(data, self.level+1)
+            self.left = t
         elif not self.right:
-            self.right = Tree(data, self.level+1)
+            self.right = t
         else:
             raise
     def __repr__(self):
-        return f"({self.left or ''}{self.data}{self.level}{self.right or ''})"
+        return f"({self.left or ''}{self.data}{self.level}[{self.parents}]{self.right or ''})"
     def __iter__(self):
         yield self
         yield from self.left
         yield from self.right
 
 def answer(in_lines):
+    start = datetime.datetime.now()
+    lut = dict()
     print('')
     top = None
     lines = set(in_lines)
@@ -37,18 +46,27 @@ def answer(in_lines):
         if not top and orbited == 'COM':
             top = Tree(orbited)
             top.add(orbits)
+            lut[orbited] = top
             found = True
         elif top:
-            for planet in top:
-                if planet.data == orbited:
-                    planet.add(orbits)
-                    found = True
+            if orbited in lut:
+                print('found')
+                lut[orbited].add(orbits)
+                found = True
+            elif True:
+                for planet in top:
+                    if planet.data == orbited:
+                        planet.add(orbits)
+                        lut[orbited] = planet
+                        found = True
         if not found:
             lines.add(l)
+    print(datetime.datetime.now()-start)
     som = 0
     for planet in top:
         print(planet)
-        som += planet.level
+        som += len(planet.parents)
+    print(datetime.datetime.now()-start)
     return som
 
 @pytest.fixture
