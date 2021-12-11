3dfd071c5b9625be8a631df7eb66f3cde94e6516
Denick <1049485+apie@users.noreply.github.com>
2021-12-11 13:30:20 +0100

    d11 cleanup

--- 2021/d11b.py
+++ 2021/d11b.py
@@ -14,72 +14,69 @@ def print_octos(oc):
     print(octo_str)
     print('')
 
-def answer(lines, STEPS=100000, NROWS=10, NCOLS=10):
+def answer(lines, STEPS=1000, NROWS=10, NCOLS=10):
+    def parse_input(lines):
+        return [
+            list(int(o) for o in list(line))
+            for line in map(str.strip, lines)
+        ]
+
+    def increase_octo_levels():
+        for i in range(NROWS):
+            for j in range(NCOLS):
+                octos[i][j] += 1
+
+    def flash(n):
+        def increase_adjacent(i, j):
+            def increase(r, c, direction):
+                if octos[r][c] <= 9 and not (r,c) in flashers:
+#                    print(f'increasing {direction}')
+                    octos[r][c] += 1
+
+            if j > 0:
+                increase(i, j-1, 'left')
+            if i > 0 and j > 0:
+                increase(i-1, j-1, 'top left diag')
+            if i > 0:
+                increase(i-1, j, 'top')
+            if i > 0 and j < NCOLS-1:
+                increase(i-1, j+1, 'top right diag')
+            if j < NCOLS-1:
+                increase(i, j+1, 'right')
+            if i < NROWS-1 and j < NCOLS-1:
+                increase(i+1, j+1, 'bottom right diag')
+            if i < NROWS-1:
+                increase(i+1, j, 'bottom')
+            if i < NROWS-1 and j > 0:
+                increase(i+1, j-1, 'bottom left diag')
+
+        # ------------------
+        flashed = False
+        for i in range(NROWS):
+            for j in range(NCOLS):
+                #any with level > 9 : FLASH
+                if octos[i][j] > 9:
+                    flashed = True
+#                        print('flashing ',i,j)
+                    n += 1
+                    if (i,j) not in flashers:#only allowed once per step
+                        #increase level of octos adjacent to flashers
+                        increase_adjacent(i, j)
+                    flashers.add((i,j))
+                    #reset this one
+                    octos[i][j] = 0
+        return not flashed, n
+    #--------------
+
     print('')
     n_flashes = 0
-    octos = []
-    for line in map(str.strip, lines):
-        octos.append(list(int(o) for o in list(line)))
+    octos = parse_input(lines)
     print_octos(octos)
     for step in range(1, STEPS+1):
         print(f'----- {step=} ------')
         flashers = set()
         #increase level for each octo
-        for i in range(NROWS):
-            for j in range(NCOLS):
-                octos[i][j] += 1
-
-        #increase level of octos adjacent to flashers
-        def increase_adjacent(i, j):
-            if j > 0:#check left
-                if octos[i][j-1] <= 9 and not (i,j-1) in flashers:
-#                    print('increasing.left')
-                    octos[i][j-1] += 1
-            if i > 0 and j > 0:#check left diag
-                if octos[i-1][j-1] <= 9 and not (i-1,j-1) in flashers:
-#                    print('increasing.left diag')
-                    octos[i-1][j-1] += 1
-            if i > 0:#check top
-                if octos[i-1][j] <= 9 and not (i-1,j) in flashers:
-#                    print('increasing.top')
-                    octos[i-1][j] += 1
-            if i > 0 and j < NCOLS-1:#check right diag
-                if octos[i-1][j+1] <= 9 and not (i-1,j+1) in flashers:
-#                    print('increasing.right diag')
-                    octos[i-1][j+1] += 1
-            if j < NCOLS-1:#check right
-                if octos[i][j+1] <= 9 and not (i,j+1) in flashers:
-                    #print('increasing.right')
-                    octos[i][j+1] += 1
-            if i < NROWS-1 and j < NCOLS-1:#check bottom right diag
-                if octos[i+1][j+1] <= 9 and not (i+1,j+1) in flashers:
-                    #print('increasing.bottom right ')
-                    octos[i+1][j+1] += 1
-            if i < NROWS-1:#check bottom
-                if octos[i+1][j] <= 9 and not (i+1,j) in flashers:
-                    #print('increasing.bottom')
-                    octos[i+1][j] += 1
-            if i < NROWS-1 and j > 0:#check bottom left diag
-                if octos[i+1][j-1] <= 9 and not (i+1,j-1) in flashers:
-                    #print('increasing.bottom left')
-                    octos[i+1][j-1] += 1
-        def flash(n):
-            flashed = False
-            #any with level > 9 : FLASH
-            for i in range(NROWS):
-                for j in range(NCOLS):
-                    if octos[i][j] > 9:
-                        flashed = True
-#                        print('flashing ',i,j)
-                        n += 1
-                        if (i,j) not in flashers:#only allowed once per step
-                            increase_adjacent(i, j)
-                        flashers.add((i,j))
-                        #reset this one
-                        octos[i][j] = 0
-            no_flashes = not flashed
-            return no_flashes, n
-
+        increase_octo_levels()
         flash_done = False
         while not flash_done:
             flash_done, n_flashes = flash(n_flashes)
