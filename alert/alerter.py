4b2b7c010813de2456fe9c89df61f26a48fdc010
Denick <1049485+apie@users.noreply.github.com>
2022-10-29 16:28:34 +0200

    PEP8 alerter.py

--- alert/alerter.py
+++ alert/alerter.py
@@ -1,26 +1,25 @@
 #!/usr/bin/env python3
-#Print a line when someone from your private leaderboard on adventofcode.com solves a puzzle. Updates at most every 15 minutes since more is not allowed.
-#By Apie 2021-12-13
+# Print a line when someone from your private leaderboard on adventofcode.com solves a puzzle. Updates at most every 15 minutes since more is not allowed.
+# By Apie 2021-12-13
 
 import os
 import requests
 from requests.exceptions import JSONDecodeError
 import json
-from datetime import datetime, timedelta, date
+from datetime import datetime, timedelta
 from pathlib import Path
 from os.path import dirname, abspath
 from time import sleep
 
 
-
-COOKIE_FILE = dirname(abspath(__file__))+'/../cookie.txt'
+COOKIE_FILE = dirname(abspath(__file__)) + '/../cookie.txt'
 with open(COOKIE_FILE) as f:
     COOKIE = f.read().strip()
 
 YEAR = 2021
 LEADERBOARD_ID = 380357
-STATS_URL = f'https://adventofcode.com/{YEAR}/leaderboard/private/view/{LEADERBOARD_ID}.json' 
-FILENAME = Path(dirname(abspath(__file__))+'/cache.txt')
+STATS_URL = f'https://adventofcode.com/{YEAR}/leaderboard/private/view/{LEADERBOARD_ID}.json'
+FILENAME = Path(dirname(abspath(__file__)) + '/cache.txt')
 
 s = requests.session()
 
@@ -28,11 +27,12 @@ s = requests.session()
 def get_from_cache() -> str:
     if not FILENAME.exists():
         return
-    if datetime.fromtimestamp(FILENAME.stat().st_mtime)+timedelta(minutes=15) < datetime.now():
+    if datetime.fromtimestamp(FILENAME.stat().st_mtime) + timedelta(minutes=15) < datetime.now():
         os.remove(FILENAME)
         return
     with open(FILENAME) as f:
         return f.read()
+
 
 def update_cache():
     r = s.get(STATS_URL, cookies=dict(session=COOKIE), timeout=5)
@@ -46,12 +46,14 @@ def update_cache():
         f.write(stats)
     return stats
 
+
 def get_stats():
     return get_from_cache() or update_cache()
 
+
 def get_done_set(m):
     return {
-        'day '+day+' part '+part
+        'day ' + day + ' part ' + part
         for day, parts in m['completion_day_level'].items()
         for part in parts
     }
@@ -65,18 +67,16 @@ if __name__ == '__main__':
         puzzles_done[name] = get_done_set(m)
 
     while True:
-        sleep(60*1)
+        sleep(60)
         stats = json.loads(get_stats())
         for m_id, m in stats['members'].items():
             done = get_done_set(m)
             name = m['name']
             if len(done) == len(puzzles_done[name]):
-                continue # nothing changed
+                continue  # nothing changed
             for p_done in sorted(done.difference(puzzles_done[name])):
                 _, day, _, part = p_done.split()
                 dt = datetime.fromtimestamp(m['completion_day_level'][day][part]['get_star_ts'])
                 print(f'[{dt.strftime("%Y-%m-%d %H:%M:%S")}] {name} solved {p_done}!', flush=True)
             # Save new state
             puzzles_done[name] = done
-
-
