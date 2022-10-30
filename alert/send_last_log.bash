1a5526d1c8ece486cbed3047a00eb2e237e3a8ca
Denick <1049485+apie@users.noreply.github.com>
2022-10-30 17:48:29 +0100

    fix alert/send_last_log.bash

--- alert/send_last_log.bash
+++ alert/send_last_log.bash
@@ -1,4 +1,5 @@
 #!/bin/bash
-set -euo pipefail
+touch log.txt
+touch sent.txt
 diff log.txt sent.txt | grep '<' | ~/.local/bin/telegram-send --config ~/telegram/send/denicksbot/oud-scintilla-aoc.conf --stdin
 cp log.txt sent.txt
