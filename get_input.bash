84fafb2d7a8ee89c6e15113e5cbc04ce4a65a781
Denick <1049485+apie@users.noreply.github.com>
2022-10-30 17:47:17 +0100

    Update get_input.bash for multiple years

--- get_input.bash
+++ get_input.bash
@@ -1,7 +1,8 @@
 #!/bin/bash
-if [ $# -eq 0 ]
+set -euo pipefail
+if [ $# -lt 2 ]
 then
-  echo 'Provide day to download input for'
+  echo 'Provide day and year to download input for'
   exit 1
 fi
 DIR=$(dirname "$0")
@@ -11,6 +12,6 @@ then
   exit 1
 fi
 COOKIE=$(cat "$DIR"/cookie.txt)
-YEAR=2021
+YEAR=$2
 DAY=$(($1)) # Convert to int to strip leading zero
 curl "https://adventofcode.com/$YEAR/day/$DAY/input" -H "Cookie: session=$COOKIE" > "$DIR"/$YEAR/d"$(printf "%.d.input" "$DAY")"
