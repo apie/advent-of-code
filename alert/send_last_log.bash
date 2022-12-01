6b61e97fe50d00ca0a7de0aa89f21013aabb7d02
Denick <1049485+apie@users.noreply.github.com>
2022-12-01 23:43:49 +0100

    Simplify alert script more

--- alert/send_last_log.bash
+++ alert/send_last_log.bash
@@ -1,5 +1,7 @@
 #!/bin/bash
-touch log.txt
-touch sent.txt
-diff log.txt sent.txt | grep '<' | ~/.local/bin/telegram-send --config ~/telegram/send/denicksbot/oud-scintilla-aoc.conf --stdin
-cp log.txt sent.txt
+set -eu
+YEAR=$1
+touch $YEAR/log.txt
+touch $YEAR/sent.txt
+diff $YEAR/log.txt $YEAR/sent.txt | grep '<' | ~/.local/bin/telegram-send --config ~/telegram/send/denicksbot/oud-scintilla-aoc.conf --stdin
+cp $YEAR/log.txt $YEAR/sent.txt
