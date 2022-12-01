ee0fa4233c79d66df9863b8e5081737c75f7a393
Denick <1049485+apie@users.noreply.github.com>
2022-12-01 23:00:30 +0100

    Simplify alert script

--- alert/alerter.py
+++ alert/alerter.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# Print a line when someone from your private leaderboard on adventofcode.com solves a puzzle. Updates at most every 15 minutes since more is not allowed.
+# Print lines when the puzzles in your private leaderboard were solved. Updates at most every 15 minutes since more is not allowed.
 # By Apie 2021-12-13
 
 import sys
@@ -18,7 +18,6 @@ with open(COOKIE_FILE) as f:
     COOKIE = f.read().strip()
 
 YEAR = sys.argv[1]
-UPDATE_FREQ_MINUTES = 1 if len(sys.argv) == 2 else int(sys.argv[2])
 LEADERBOARD_ID = 380357
 STATS_URL = f'https://adventofcode.com/{YEAR}/leaderboard/private/view/{LEADERBOARD_ID}.json'
 FILENAME = Path(dirname(abspath(__file__)) + f'/cache_{YEAR}.txt')
@@ -26,10 +25,10 @@ FILENAME = Path(dirname(abspath(__file__)) + f'/cache_{YEAR}.txt')
 s = requests.session()
 
 
-def get_from_cache(remove_old_cache=True) -> str:
+def get_from_cache() -> str:
     if not FILENAME.exists():
         return
-    if remove_old_cache and datetime.fromtimestamp(FILENAME.stat().st_mtime) + timedelta(minutes=15) < datetime.now():
+    if datetime.fromtimestamp(FILENAME.stat().st_mtime) + timedelta(minutes=15) < datetime.now():
         os.remove(FILENAME)
         return
     with open(FILENAME) as f:
@@ -49,37 +48,16 @@ def update_cache():
     return stats
 
 
-def get_stats(remove_old_cache=True):
-    return get_from_cache(remove_old_cache) or update_cache()
-
-
-def get_done_set(m):
-    return {
-        'day ' + day + ' part ' + part
-        for day, parts in m['completion_day_level'].items()
-        for part in parts
-    }
+def get_stats():
+    return get_from_cache() or update_cache()
 
 
 if __name__ == '__main__':
-    puzzles_done = dict()
-    # Keep old cache so that we get notified of all the differences since the last time.
-    stats = json.loads(get_stats(remove_old_cache=False))
-    for m_id, m in stats['members'].items():
-        name = m['name']
-        puzzles_done[name] = get_done_set(m)
-
-    while True:
-        sleep(60 * UPDATE_FREQ_MINUTES)
-        stats = json.loads(get_stats())
-        for m_id, m in stats['members'].items():
-            done = get_done_set(m)
-            name = m['name']
-            if len(done) == len(puzzles_done[name]):
-                continue  # nothing changed
-            for p_done in sorted(done.difference(puzzles_done[name])):
-                _, day, _, part = p_done.split()
+    done_lines = []
+    stats = json.loads(get_stats())
+    for m in stats['members'].values():
+        for day, parts in m['completion_day_level'].items():
+            for part in parts:
                 dt = datetime.fromtimestamp(m['completion_day_level'][day][part]['get_star_ts'])
-                print(f'[{dt.strftime("%Y-%m-%d %H:%M:%S")}] {name} solved {YEAR} {p_done}!', flush=True)
-            # Save new state
-            puzzles_done[name] = done
+                done_lines.append(f'[{dt.strftime("%Y-%m-%d %H:%M:%S")}] {m["name"]} solved {YEAR} day {day} part {part}!')
+    print('\n'.join(sorted(done_lines)))
---
+++ alert/readme.md
@@ -0,0 +1,2 @@
+Add cronjobs for alerter.py <year> and redirect the output to a file.
+Use start.bash to watch the file and send differences to telegram.
