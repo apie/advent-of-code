8c76b80654d014f88accbd3c08c3a518b9cdfb4e
Denick <1049485+apie@users.noreply.github.com>
2022-12-05 23:32:48 +0100

    day 5

---
+++ 2022/d5a.pas
@@ -0,0 +1,59 @@
+PROGRAM d5a;
+USES sysutils;
+
+FUNCTION answer(filename:string) : string;
+VAR
+    f: text;
+    l: string;
+    i, amt, src, dest: integer;
+    stack: array[1..9] of string;
+BEGIN
+    answer := '';
+    FOR i := 1 TO 9 DO stack[i] := '';
+    assign(f, filename);
+    reset(f);
+    WHILE not eof(f) DO
+    BEGIN
+        {read stacks}
+        readln(f, l);
+        IF pos(' 1 ', l) > 0 THEN break;
+        FOR i := 1 TO 9 DO stack[i] += trim(copy(l, -2+4*i, 1));
+    END;
+    readln(f, l);
+    WHILE not eof(f) DO
+    BEGIN
+        {read instruction}
+        readln(f, l);
+        amt := strtoint(trim(copy(l, pos('move', l)+5, 2)));
+        src := strtoint(trim(copy(l, pos('from', l)+5, 2)));
+        dest := strtoint(trim(copy(l, pos('to', l)+3, 1)));
+        FOR i := 1 TO amt DO
+        BEGIN
+            IF (length(stack[src]) > 0) THEN
+            BEGIN
+                stack[dest] := stack[src][1] + stack[dest];
+                stack[src] := copy(stack[src], 2, length(stack[src]));
+            END;
+        END;
+    END;
+    close(f);
+    FOR i := 1 TO 9 DO
+    BEGIN
+        IF (length(stack[i]) > 0) THEN answer += trim(stack[i][1]);
+    END;
+    answer := trim(answer);
+END;
+
+CONST
+    testfile = 'd5.test.1';
+    filename = 'd5.input';
+VAR
+    a: string;
+BEGIN{d5a}
+    assert(answer(testfile) = 'CMZ', 'test faal');
+    a := answer(filename);
+    assert(a <> 'CMZWCZTHTMPS');
+    assert(a <> 'WCZTHTMPSWCZTHTMPS');
+    writeln('');
+    writeln('answer: ', answer(filename));
+END.
