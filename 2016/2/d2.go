3eb0659726da0fd1214bad13454b078e1e2e470e
Denick <1049485+apie@users.noreply.github.com>
2023-10-08 18:01:35 +0200

    2016 day 2 using nested array

--- 2016/2/d2.go
+++ 2016/2/d2.go
@@ -3,85 +3,67 @@ package main
 import (
 	"fmt"
 	"os"
-	"slices"
 	"strings"
 )
 
 func D2a(filename string) string {
-	content, err := os.ReadFile(filename) // the file is inside the local directory
-	if err != nil {
-		fmt.Println("Err")
+	keypad := [][]int{
+		[]int{1, 2, 3},
+		[]int{4, 5, 6},
+		[]int{7, 8, 9},
 	}
-	code := ""
-	curnum := 5
-	lines := strings.Split(strings.TrimSpace(string(content)), "\n")
-	l := []int{1, 4, 7}
-	r := []int{3, 6, 9}
-	for _, line := range lines {
-		fmt.Println(line)
-		for _, char := range strings.Split(line, "") {
-			//fmt.Println(char)
-			if char == "U" && curnum-3 > 0 {
-				curnum -= 3
-			} else if char == "D" && curnum+3 < 10 {
-				curnum += 3
-				// need Go >= 1.21
-			} else if char == "L" && !slices.Contains(l, curnum) {
-				curnum -= 1
-			} else if char == "R" && !slices.Contains(r, curnum) {
-				curnum += 1
-			}
-		}
-		code = fmt.Sprintf("%s%d", code, curnum)
-		fmt.Println(code)
-	}
-	return code
+	startx := 1
+	starty := 1
+	return solve_keypad(filename, keypad, startx, starty)
 }
 func D2b(filename string) string {
+	keypad := [][]int{
+		[]int{0, 0, 1, 0, 0},
+		[]int{0, 2, 3, 4, 0},
+		[]int{5, 6, 7, 8, 9},
+		[]int{0, 10, 11, 12, 0},
+		[]int{0, 0, 13, 0, 0},
+	}
+	startx := 0
+	starty := 2
+	return solve_keypad(filename, keypad, startx, starty)
+}
+func solve_keypad(filename string, keypad [][]int, startx int, starty int) string {
 	content, err := os.ReadFile(filename) // the file is inside the local directory
 	if err != nil {
 		fmt.Println("Err")
 	}
-	//    1
-	//  2 3 4
-	//5 6 7 8 9
-	//  A B C
-	//    D
 	code := ""
-	curnum := 5
+	x := startx
+	y := starty
+	maxx := len(keypad[0]) - 1
+	maxy := len(keypad) - 1
 	lines := strings.Split(strings.TrimSpace(string(content)), "\n")
-	fmt.Println(lines)
-	l := []int{1, 2, 5, 10, 13}
-	r := []int{1, 4, 9, 12, 13}
-	u := []int{5, 2, 1, 4, 9}
-	d := []int{5, 10, 13, 12, 9}
-	up2 := []int{3, 13}
-	down2 := []int{1, 11}
 	for _, line := range lines {
-		fmt.Println(line)
+		//fmt.Println(line)
 		for _, char := range strings.Split(line, "") {
-			// need Go >= 1.21
-			if char == "U" && !slices.Contains(u, curnum) {
-				if slices.Contains(up2, curnum) {
-					curnum -= 2
-				} else {
-					curnum -= 4
+			//fmt.Println(char)
+			if char == "U" {
+				if y > 0 && keypad[y-1][x] > 0 {
+					y -= 1
 				}
-			} else if char == "D" && !slices.Contains(d, curnum) {
-				if slices.Contains(down2, curnum) {
-					curnum += 2
-				} else {
-					curnum += 4
+			} else if char == "D" {
+				if y < maxy && keypad[y+1][x] > 0 {
+					y += 1
 				}
-			} else if char == "L" && !slices.Contains(l, curnum) {
-				curnum -= 1
-			} else if char == "R" && !slices.Contains(r, curnum) {
-				curnum += 1
+			} else if char == "L" {
+				if x > 0 && keypad[y][x-1] > 0 {
+					x -= 1
+				}
+			} else if char == "R" {
+				if x < maxx && keypad[y][x+1] > 0 {
+					x += 1
+				}
 			}
+			//fmt.Println(x*10+y)
 		}
-		fmt.Println(curnum)
-		code = fmt.Sprintf("%s%X", code, curnum) // Append hex representation of code
-		fmt.Println(code)
+		code = fmt.Sprintf("%s%X", code, keypad[y][x])
+		//fmt.Println(code)
 	}
 	return code
 }
