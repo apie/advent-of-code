3f118a450738655f424e8abd272bf630ad7b227d
Denick <1049485+apie@users.noreply.github.com>
2023-10-30 18:20:42 +0100

    create dirs

--- alert/update_and_send.bash
+++ alert/update_and_send.bash
@@ -3,5 +3,7 @@ set -eu
 LEADERBOARD_ID=$1
 YEAR=$2
 CONFIGFILE=$3
+mkdir -p $LEADERBOARD_ID/$YEAR
+touch $LEADERBOARD_ID/$YEAR/log.txt
 ./alerter.py $YEAR $LEADERBOARD_ID > $LEADERBOARD_ID/$YEAR/log.txt && ./send_last_log.bash $LEADERBOARD_ID $YEAR $CONFIGFILE
 
