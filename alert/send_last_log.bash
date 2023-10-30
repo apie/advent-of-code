a11800e51c976e2472d99d7cbd3c6b63e8b33b0a
Denick <1049485+apie@users.noreply.github.com>
2023-10-30 23:12:31 +0100

    absolute path

--- alert/send_last_log.bash
+++ alert/send_last_log.bash
@@ -6,5 +6,5 @@ CONFIGFILE=$3
 mkdir -p $LEADERBOARD_ID/$YEAR
 touch $LEADERBOARD_ID/$YEAR/log.txt
 touch $LEADERBOARD_ID/$YEAR/sent.txt
-diff $LEADERBOARD_ID/$YEAR/log.txt $LEADERBOARD_ID/$YEAR/sent.txt | grep '<' | telegram-send --config $CONFIGFILE --stdin
+diff $LEADERBOARD_ID/$YEAR/log.txt $LEADERBOARD_ID/$YEAR/sent.txt | grep '<' | ~/.local/bin/telegram-send --config $CONFIGFILE --stdin
 cp $LEADERBOARD_ID/$YEAR/log.txt $LEADERBOARD_ID/$YEAR/sent.txt
