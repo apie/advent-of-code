8b0a6a6b6182a79dc1c57f1161124d0a81064c3f
Denick <1049485+apie@users.noreply.github.com>
2023-12-06 08:05:41 +0100

    d05 wip

--- 2023/d05b.py
+++ 2023/d05b.py
@@ -17,7 +17,7 @@ def parse_map(line_iter):
             line = next(line_iter)
         except StopIteration:
             line = ''
-    return parsed_map
+    return sorted(parsed_map)
 
 def answer(lines):
     linez = map(str.strip, lines)
@@ -40,8 +40,93 @@ def answer(lines):
     print()
     answer = 99999999999999999
 
+    print('location we are looking for must be lower than lower bound of location numbers in the input:')
+    print(maps[-1][0][0])
+    upper_bound = maps[-1][0][0]
+    print('search for a humidity based on that upper bound')
+    humiditys=maps[-2]
+    humidityfound=False
+    for humidity in humiditys:
+        if humidity[0] <= upper_bound <= humidity[0] + humidity[2]:
+            humidityfound=humidity
+            print(humidity)
+    if humidityfound:
+        upper_bound = upper_bound - humidityfound[0] + humidityfound[1]
+        print('new upper bound:', upper_bound)
+    else:
+        print('humidity not found, use same')
+            
+    print('search for a temp based on that upper bound')
+    temps=maps[-3]
+    tempfound=False
+    for temp in temps:
+        if temp[0] <= upper_bound <= temp[0] + temp[2]:
+            tempfound=temp
+            print(temp)
+    if tempfound:
+        upper_bound = upper_bound - tempfound[0] + tempfound[1]
+        print('new upper bound:', upper_bound)
+    else:
+        print('temp not found, use same')
+            
+    print('search for a light based on that upper bound')
+    lights=maps[-4]
+    lightfound=False
+    for light in lights:
+        if light[0] <= upper_bound <= light[0] + light[2]:
+            lightfound=light
+            print(light)
+    if lightfound:
+        upper_bound = upper_bound - lightfound[0] + lightfound[1]
+        print('new upper bound:', upper_bound)
+    else:
+        print('lightfound not found, use same')
+            
+    print('search for a water based on that upper bound')
+    water=maps[-5]
+    waterfound=False
+    for w in water:
+        if w[0] <= upper_bound <= w[0] + w[2]:
+            waterfound=w
+            print(w)
+    if waterfound:
+        upper_bound = upper_bound - waterfound[0] + waterfound[1]
+        print('new upper bound:', upper_bound)
+    else:
+        print('waterfound not found, use same')
 
-        
+            
+    print('search for a fertilizer based on that upper bound')
+    fertilizer=maps[-6]
+    fertilizerfound=False
+    for w in fertilizer:
+        if w[0] <= upper_bound <= w[0] + w[2]:
+            fertilizerfound=w
+            print(w)
+    if fertilizerfound:
+        upper_bound = upper_bound - fertilizerfound[0] + fertilizerfound[1]
+        print('new upper bound:', upper_bound)
+    else:
+        print('fertilizerfound not found, use same')
+
+            
+    print('search for a soil based on that upper bound')
+    soil=maps[-7]
+    soilfound=False
+    for w in soil:
+        if w[0] <= upper_bound <= w[0] + w[2]:
+            soilfound=w
+            print(w)
+    if soilfound:
+        upper_bound = upper_bound - soilfound[0] + soilfound[1]
+        print('new upper bound:', upper_bound)
+    else:
+        print('soilfound not found, use same')
+
+            
+
+
+    upper_bound += 1
 
     while seeds:
         start = seeds.pop(0)
@@ -49,14 +134,18 @@ def answer(lines):
 #        print(f'{seed_range=}')
 #        breakpoint()
         for n in range(*seed_range):
+            if n > upper_bound:
+                continue
 
             @lru_cache(maxsize=None)
             def getmin(n):
                 for i, m in enumerate(maps):
-                    if i == 6:
-                        print(f'{seed_range=} {i=} {n=} {answer=}')
+#                    if i == 6:
+#                        print(f'{seed_range=} {i=} {n=} {answer=}')
                     found = False
-                    for ma in m:
+                    ma = m[0]
+                    if True:
+#                    for ma in m:
                         if found:
                             break
                         dest_range_start, source_range_start, range_length = ma
@@ -88,6 +177,8 @@ if __name__ == '__main__':
     assert ans < 1617282249
     assert ans < 403695602
     assert ans < 219538664
+    assert ans != 629551616
+    assert ans != 105822591
     print('Answer:', ans)
     duration = timeit.default_timer()-start
     print(f'Execution time: {duration:.3f} s')
