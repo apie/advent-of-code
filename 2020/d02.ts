7fe87706f13a779761d3f0fc6c814a782d67f3d9
Denick <1049485+apie@users.noreply.github.com>
2024-10-31 11:34:09 +0100

    Refactor d2

--- 2020/d02.ts
+++ 2020/d02.ts
@@ -9,19 +9,17 @@ function parsePassword(password: string): RegExpMatchArray {
     }
     return parsedPassword;
 }
-function checkPassword(password: string): boolean {
-    // console.log(password);
-    const parsedPassword = password.match(
-        /(?<from>\d+)-(?<to>\d+) (?<letter>\w): (?<hay>\w+)/,
-    );
-    if (!parsedPassword?.groups) {
-        throw Error("unknown line found");
-    }
+function checkPasswordPart1(
+    word: string,
+    letter: string,
+    posOne: number,
+    posTwo: number,
+): boolean {
     let numMatched = 0;
-    const passwordArray = parsedPassword.groups.hay.split("");
+    const passwordArray = word.split("");
     let letterIndex = -1;
     while (numMatched <= passwordArray.length) {
-        letterIndex = passwordArray.indexOf(parsedPassword.groups.letter);
+        letterIndex = passwordArray.indexOf(letter);
         if (letterIndex > -1) {
             passwordArray[letterIndex] = ".";
             numMatched += 1;
@@ -29,33 +27,32 @@ function checkPassword(password: string): boolean {
             break;
         }
     }
-    if (numMatched > Number(parsedPassword.groups.to)) return false;
-    if (numMatched < Number(parsedPassword.groups.from)) return false;
+    if (numMatched > Number(posTwo)) return false;
+    if (numMatched < Number(posOne)) return false;
     return true;
 }
-export function part1(d02_input: string): number {
-    const passwords = d02_input.trim().split("\n");
-    return passwords.filter((pw) => pw[0].match(/\d/)).map((pw) =>
-        checkPassword(pw)
-    ).reduce(
-        (total: number, result: boolean) => total += Number(result),
-        0,
-    );
-}
+
 function checkPasswordPart2(
     word: string,
     letter: string,
     posOne: number,
     posTwo: number,
-) {
+): boolean {
     return Boolean(
         Number(word[posOne - 1] === letter) ^
             Number(word[posTwo - 1] === letter),
     );
 }
-
-export function part2(d02_input: string): number {
-    const passwords = d02_input.trim().split("\n");
+export function solution(
+    input: string,
+    checkPasswordFunction: (
+        word: string,
+        letter: string,
+        posOne: number,
+        posTwo: number,
+    ) => boolean,
+): number {
+    const passwords = input.trim().split("\n");
     return sum(
         passwords.filter((pw) => pw[0].match(/\d/)).map((pw) => {
             const parsedPassword = parsePassword(pw);
@@ -64,12 +61,21 @@ export function part2(d02_input: string): number {
             const posTwo = Number(parsedPassword.groups.to);
             const letter = parsedPassword.groups.letter;
             const word = parsedPassword.groups.hay;
-            return checkPasswordPart2(word, letter, posOne, posTwo);
+            return checkPasswordFunction(word, letter, posOne, posTwo);
         }),
     );
 }
+
+export const part1 = (input: string): number =>
+    solution(input, checkPasswordPart1);
+export const part2 = (input: string): number =>
+    solution(input, checkPasswordPart2);
+
 function d02(d02_input: string): number[] {
-    return [part1(d02_input), part2(d02_input)];
+    return [
+        part1(d02_input),
+        part2(d02_input),
+    ];
 }
 
 export default d02;
--- 2020/util.ts
+++ 2020/util.ts
@@ -1,6 +1,5 @@
-export function sum(arr: number[] | boolean[]): number {
-    return arr.reduce(
+export const sum = (arr: number[] | boolean[]): number =>
+    arr.reduce(
         (total: number, item: number | boolean) => total += Number(item),
         0,
     );
-}
