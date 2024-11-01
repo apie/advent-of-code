017a1408a9358baf4ea243d87f37558827ff586e
Denick <1049485+apie@users.noreply.github.com>
2024-11-01 15:35:45 +0100

    add typescript templates

---
+++ 2020/TEMPLATE.ts
@@ -0,0 +1,17 @@
+export const part1 = (lines: number[]): number => {
+    return 0;
+};
+export const part2 = (lines: number[]): number => {
+    return 0;
+};
+
+function d01(input: string): number[] {
+    const lines = input.trim().split("\n").filter((line) => line.match(/^\d/))
+        .map((line) => Number(line));
+    return [
+        part1(lines),
+        part2(lines),
+    ];
+}
+
+export default d01;
---
+++ 2020/TEST_TEMPLATE.ts
@@ -0,0 +1,12 @@
+import { assertEquals } from "jsr:@std/assert";
+
+import { part1, part2 } from "./d01.ts";
+
+const testinput = `
+`.trim().split("\n").map((line) => Number(line));
+Deno.test("part 1", () => {
+  assertEquals(part1(testinput), -1);
+});
+// Deno.test("part 2", () => {
+//   assertEquals(part2(testinput), -1);
+// });
