3e027025126aeb4106e951b71d96d8777cf30228
Denick <1049485+apie@users.noreply.github.com>
2024-11-03 14:55:39 +0100

    print curpos

--- 2020/d03.ts
+++ 2020/d03.ts
@@ -1,22 +1,28 @@
 const printMap = (lines: string[], pos: { x: number; y: number }): void => {
     lines.forEach((line, y) => {
+        let lineArr = line.split("");
+        while (pos.x > lineArr.length) {
+            lineArr = lineArr.concat(lineArr);
+        }
         if (pos.y === y) {
-            const lineArr = line.split("");
-            if (line[pos.x] === "#") {
+            if (lineArr[pos.x] === "#") {
                 lineArr[pos.x] = "X";
             } else {
                 lineArr[pos.x] = "O";
             }
             console.log(lineArr.join(""));
-        } else console.log(line);
+        } else console.log(lineArr.join(""));
     });
+    console.log();
 };
 export const part1 = (lines: string[]): number => {
     const startPos = { x: 0, y: 0 };
-    let pos = startPos;
-    pos.x += 3;
-    pos.y += 1;
-    printMap(lines, pos);
+    const pos = startPos;
+    while (pos.y < lines.length -1) {
+        pos.x += 3;
+        pos.y += 1;
+        printMap(lines, pos);
+    }
     return 0;
 };
 export const part2 = (lines: string[]): number => {
