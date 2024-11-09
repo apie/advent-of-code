84b805e73ef7f8506d55f81669881c46c5067155
Denick <1049485+apie@users.noreply.github.com>
2024-11-09 13:46:47 +0100

    cleanup

--- 2020/d05.ts
+++ 2020/d05.ts
@@ -2,13 +2,11 @@ import { assert } from "jsr:@std/assert/assert";
 import "./util.ts";
 
 export const getseatID = (boardingPass: string): number => {
-    console.log("*" + boardingPass + "*");
-    const boardingPassA = boardingPass.split("");
-    let row = [0, 127];
-    // console.log(row);
-    while (boardingPassA.length > 3) {
-        const char = boardingPassA.shift();
-        // console.log(char);
+    // console.log("*" + boardingPass + "*");
+    const boardingPassArr = boardingPass.split("");
+    const row = [0, 127];
+    while (boardingPassArr.length > 3) {
+        const char = boardingPassArr.shift();
         switch (char) {
             case "F":
                 row[1] = row[1] - (1 + row[1] - row[0]) / 2;
@@ -19,14 +17,11 @@ export const getseatID = (boardingPass: string): number => {
             default:
                 throw Error("Unknown character " + char);
         }
-        // console.log(row);
     }
     assert(row[0] === row[1], "Didnt find row");
-    let col = [0, 7];
-    // console.log(col);
-    while (boardingPassA.length) {
-        const char = boardingPassA.shift();
-        // console.log(char);
+    const col = [0, 7];
+    while (boardingPassArr.length) {
+        const char = boardingPassArr.shift();
         switch (char) {
             case "L":
                 col[1] = col[1] - (1 + col[1] - col[0]) / 2;
@@ -37,37 +32,30 @@ export const getseatID = (boardingPass: string): number => {
             default:
                 throw Error("Unknown character " + char);
         }
-        // console.log(col);
     }
     assert(col[0] === col[1], "Didnt find col");
-
     return row[0] * 8 + col[0];
 };
-export const part1 = (lines: string[]): number => {
-    return lines.map((line) => getseatID(line)).max();
-};
+
+export const part1 = (lines: string[]): number =>
+    lines.map((line) => getseatID(line)).max();
 
 const findMissingSeatID = (lines: string[]): number => {
-    const seatIds = lines.map((line) => {
+    const mySeatArr = lines.map((line) => {
         return getseatID(line);
-    }).sort();
-    const seatSet = new Set(seatIds);
-    let mySeatArr = seatIds.filter((sId) => {
-        if (seatSet.has(sId) && seatSet.has(sId - 1) && seatSet.has(sId + 1)) {
-            return;
-        }
-        return sId;
-    });
-    mySeatArr = mySeatArr.filter((sId) => {
-        return mySeatArr.includes(sId - 2);
-    });
-    assert(mySeatArr.length === 1);
+    }).filter((sId, _i, arr) =>
+        // Filter out three in a row
+        !(
+            arr.includes(sId) && arr.includes(sId - 1) && arr.includes(sId + 1)
+        )
+        // Find candidate upper neighbour
+    ).filter((sId, _id, arr) => arr.includes(sId - 2));
+    assert(mySeatArr.length === 1, "Found multiple seats");
     const mySeat = mySeatArr[0] - 1;
     return mySeat;
 };
-export const part2 = (lines: string[]): number => {
-    return findMissingSeatID(lines);
-};
+
+export const part2 = (lines: string[]): number => findMissingSeatID(lines);
 
 function d05(input: string): number[] {
     const lines = input.trim().split("\n").filter((line) => line.match(/\w/));
