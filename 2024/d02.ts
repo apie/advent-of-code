70a562d0bc38a2889e87732e0e3990ea0c812725
Denick <1049485+apie@users.noreply.github.com>
2024-12-02 22:57:41 +0100

    d02a

--- 2024/d02.ts
+++ 2024/d02.ts
@@ -6,23 +6,34 @@ const parseReport = (unparsedReport: string): Treport =>
     unparsedReport.split(" ").map((char) => Number(char));
 
 const isReportSafe = (report: Treport): boolean => {
-    //TODO
-    return false;
+    console.log(report);
+    let totsign = 0;
+    const a = report.reduce((prev, val) => {
+        const diff = prev - val;
+        const sign = diff / Math.abs(diff);
+        console.log(prev, val, "sign", sign);
+        totsign += sign;
+        return [1, 2, 3].includes(Math.abs(diff)) ? val : NaN;
+    });
+    console.debug("totsign", totsign, a);
+    if (Number.isNaN(a)) return false;
+    if (Math.abs(totsign) !== report.length - 1) return false;
+    return true;
 };
 export const part1 = (lines: string[]): number => {
     // Check which reports are safe:
     // Either all increasing or all decreasing
     // Any two adjacent levels differ by at least one and at most three .
     console.log(lines);
-    const unparsedReport = lines[0];
-    console.log(unparsedReport);
-    const report = parseReport(unparsedReport);
-    console.log(report);
-    console.log(isReportSafe(report));
+    // const unparsedReport = lines[0];
+    // console.log(unparsedReport);
+    // const report = parseReport(unparsedReport);
+    // console.log(report);
+    // console.log(isReportSafe(report));
+    // return 0;
     const ans = lines.map((line) => parseReport(line)).map((report) =>
         isReportSafe(report)
-    )
-        .count(true);
+    ).count(true);
     console.log(ans);
     return ans;
 };
