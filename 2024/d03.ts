2548e479c94c7f377334096d1440f229e4a8f713
Denick <1049485+apie@users.noreply.github.com>
2024-12-03 11:15:59 +0100

    d03b

--- 2024/d03.ts
+++ 2024/d03.ts
@@ -19,7 +19,27 @@ export const part1 = (lines: string[]): number => {
     }).sum();
 };
 export const part2 = (lines: string[]): number => {
-    return 0;
+    let enabled = true;
+    // Same as before but only in between do() and don't() instructions
+    return lines.map((line) => {
+        const m = line.match(/do\(\)|don't\(\)|mul\(\d{1,3},\d{1,3}\)/g);
+        console.log("m=", m);
+        if (!m) return 0;
+        const multiplied = m.map((expr) => {
+            // console.log(enabled, "expr", expr);
+            if (expr.match(/do\(\)/)) enabled = true;
+            else if (expr.match(/don't\(\)/)) enabled = false;
+            if (!enabled) return 0;
+            const j = expr.match(/(\d+),(\d+)/);
+            // console.log(expr);
+            if (!j) return 0;
+            // console.log(j[2])
+            return Number(j[1]) * Number(j[2]);
+        });
+        console.log(multiplied);
+        console.log(multiplied.sum());
+        return multiplied.sum();
+    }).sum();
 };
 
 function d03(input: string): number[] {
