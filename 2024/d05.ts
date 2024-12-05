fc7ff97c6c163b2de7f93e40f1e519c768340962
Denick <1049485+apie@users.noreply.github.com>
2024-12-05 17:10:58 +0100

    d05a

--- 2024/d05.ts
+++ 2024/d05.ts
@@ -8,14 +8,25 @@ const pageUpdateValid = (
     rules: { [x: number]: number }[],
     pageUpdate: number[],
 ): boolean => {
-    //FIXME
-    const s = [...pageUpdate].sort((a, b) => {
-        const arule = rules.find((rule) => rule[a] !== undefined);
-        if (!arule) return 0;
-        return arule[a] === b ? -1 : 1;
+    // console.log();
+    const s = [...pageUpdate].sort((second, first) => {
+        const arule = rules.filter((rule) =>
+            (Object.values(rule).includes(second)) ||
+            (Object.values(rule).includes(first)) ||
+            (rule[first] !== undefined)
+        );
+        // console.debug("first", first, "second", second, "arule", arule);
+        if (arule.length === 0) return 0;
+        let retval = 1;
+        arule.forEach((rule) => {
+            // console.log(rule[second] === first);
+            if (rule[second] === first) retval = -1;
+        });
+        return retval;
     });
-    console.log("n", pageUpdate);
-    console.log("s", s);
+    // console.log("n", pageUpdate);
+    // console.log("s", s);
+    // console.log(pageUpdate.join(",") === s.join(",") ? "EQUAL" : "NOT EQUAL");
     return pageUpdate.join(",") === s.join(",");
 };
 export const part1 = (lines: string[]): number => {
@@ -25,18 +36,20 @@ export const part1 = (lines: string[]): number => {
         const pageNr = rule.split("|").map((n) => Number(n));
         return { [pageNr[0]]: pageNr[1] };
     });
-    console.log(rules);
+    // console.log(rules);
 
     const pageUpdates = pagesStr.split("\n").map((pageUpdateStr) =>
         pageUpdateStr.split(",").map((page) => Number(page))
     );
-    assertEquals(pageUpdateValid(rules, pageUpdates[0]), true, "0");
-    assertEquals(pageUpdateValid(rules, pageUpdates[1]), true, "1");
-    assertEquals(pageUpdateValid(rules, pageUpdates[2]), true, "2");
-    assertEquals(pageUpdateValid(rules, pageUpdates[3]), false, "3");
-    assertEquals(pageUpdateValid(rules, pageUpdates[4]), false, "4");
-    assertEquals(pageUpdateValid(rules, pageUpdates[5]), false, "5");
-    // return -1; // FIXME
+    if (pageUpdates[0].join(",") === "75,47,61,53,29") {
+        assertEquals(pageUpdateValid(rules, pageUpdates[0]), true, "z0");
+        assertEquals(pageUpdateValid(rules, pageUpdates[1]), true, "1");
+        assertEquals(pageUpdateValid(rules, pageUpdates[2]), true, "2");
+        assertEquals(pageUpdateValid(rules, pageUpdates[3]), false, "3");
+        assertEquals(pageUpdateValid(rules, pageUpdates[4]), false, "4");
+        assertEquals(pageUpdateValid(rules, pageUpdates[5]), false, "5");
+        // return -1; // FIXME
+    }
 
     return pagesStr.split("\n").map((pageUpdateStr) =>
         pageUpdateStr.split(",").map((page) => Number(page))
